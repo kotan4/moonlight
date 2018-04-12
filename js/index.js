a727b713895cae1cc566912e8f75ce95eca1bce6
Kotan <Kotan@aengine.ru>
2018-04-12 18:23:19 +0300

    v0.3

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ audioPlayer();
 function audioPlayer() {
     $("#audioPlayer")[0].src = $("#playList a")
     [0];
-    $("#audioPlayer")[0].play();
+    // $("#audioPlayer")[0].play();
     $("#playList a").click(function(e){
         e.preventDefault();
         $("#audioPlayer")[0].src = this;
@@ -15,3 +15,31 @@ function audioPlayer() {
     })
     
 }
+
+
+var animationEnd = (function(el) {
+    var animations = {
+        animation: 'animationend',
+        OAnimation: 'oAnimationEnd',
+        MozAnimation: 'mozAnimationEnd',
+        WebkitAnimation: 'webkitAnimationEnd',
+    };
+
+    for (var t in animations) {
+        if (el.style[t] !== undefined) {
+            return animations[t];
+        }
+    }
+
+})(document.createElement('div'));
+
+$('.musicIcon').on('click', function () {
+    $('.musicIcon')[0].src = this;
+        $(this).addClass('animated pulse').one(animationEnd,
+            function () {
+                $(this).removeClass('animated pulse');
+
+            });
+
+    });
+
