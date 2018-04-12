3ed860b08be0fdc88edfd20ff8b3b5b1ab17de21
Kotan <Kotan@aengine.ru>
2018-04-13 02:28:01 +0300

    v0.5

--- js/index.js
+++ js/index.js
@@ -1,45 +1,64 @@
-$('.menu-icon').click(function(){
+$('.menu-icon').click(function() {
     $('.menu').toggle("active");
 });
 
-
 audioPlayer();
 function audioPlayer() {
-    $("#audioPlayer")[0].src = $("#playList a")
-    [0];
-    // $("#audioPlayer")[0].play();
-    $("#playList a").click(function(e){
+    $("#audioPlayer")[0].src = $("#playList a")[0];
+    $("#playList a").click(function(e) {
         e.preventDefault();
         $("#audioPlayer")[0].src = this;
         $("#audioPlayer")[0].play();
-    })
-    
+    });
 }
-
-
+;
 const animationEnd = (function(el) {
-    const animations = {
-        animation: 'animationend',
-        OAnimation: 'oAnimationEnd',
-        MozAnimation: 'mozAnimationEnd',
-        WebkitAnimation: 'webkitAnimationEnd',
-    };
+        const animations = {
+            animation: 'animationend',
+            OAnimation: 'oAnimationEnd',
+            MozAnimation: 'mozAnimationEnd',
+            WebkitAnimation: 'webkitAnimationEnd',
+        };
 
-    for (const t in animations) {
-        if (el.style[t] !== undefined) {
-            return animations[t];
+        for (const t in animations) {
+            if (el.style[t] !== undefined) {
+                return animations[t];
+            }
         }
+
     }
+)(document.createElement('div'));
 
-})(document.createElement('div'));
-
-$('.musicIcon').on('click', function () {
+$('.musicIcon').on('click', function() {
     $('.musicIcon')[0].src = this;
-        $(this).addClass('animated pulse').one(animationEnd,
-            function () {
-                $(this).removeClass('animated pulse');
-
-            });
+    $(this).addClass('animated pulse').one(animationEnd, function() {
+        $(this).removeClass('animated pulse');
 
     });
 
+});
+
+// $(() => {
+//     let audio = new Audio(),
+//     setAudio = ($el) => {
+//     $el.parents('#playList').find('.beat').removeClass('beat');
+// $el.addClass('beat');
+//
+// audio.oncanplay = () => {
+//     audio.oncanplay = null;
+//     audio.play();
+// }
+//
+// audio.src = $el.attr('href');
+// audio.load();
+//
+// };
+//
+// $('a.musicIcon').on('click', (e) => {
+//     e.preventDefault();
+//
+// setAudio($(e.currentTarget));
+// });
+//
+// setAudio($('a.musicIcon').first());
+// })
