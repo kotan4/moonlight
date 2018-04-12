261ef933c385e864a20cbfe62e1b1dde0c0fff7c
Kotan <Kotan@aengine.ru>
2018-04-12 17:16:23 +0300

    rd

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 $('.menu-icon').click(function(){
     $('.menu').toggle("active");
 });
+
+
+audioPlayer();
+function audioPlayer() {
+    $("#audioPlayer")[0].src = $("#playList a")
+    [0];
+    $("#audioPlayer")[0].play();
+    $("#playList a").click(function(e){
+        e.preventDefault();
+        $("#audioPlayer")[0].src = this;
+        $("#audioPlayer")[0].play();
+    })
+    
+}
